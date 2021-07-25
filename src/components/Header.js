1b18995e36101aa4051867e384a5f4a83bd997d6
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-07-25 19:20:57 +0200

    Show Add or  Close button when location.pathname === "/"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, showAddTask, onAdd }) => {
+    const location = useLocation();
     return (
         <div className="row">
             <div className="col fw-bolder fs-2">{title} </div>
             <div className="col text-end">
-                <Button
-                    cssButton={`mt-1 btn-${showAddTask ? "danger" : "success"}`}
-                    title={`${showAddTask ? "Close" : "Add"}`}
-                    onClick={onAdd}
-                />
+                {location.pathname === "/" && (
+                    <Button
+                        cssButton={`mt-1 btn-${
+                            showAddTask ? "danger" : "success"
+                        }`}
+                        title={`${showAddTask ? "Close" : "Add"}`}
+                        onClick={onAdd}
+                    />
+                )}
             </div>
         </div>
     );
