17e251536979dfcce8a9ac54b1b399b7f7607964
Kezimana Aimé Angelo <kezangelo@gmail.com>
2021-07-25 16:51:43 +0200

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="text-center">
+            Copyright &copy; 2021 <br />
+            <button className="btn btn-sm btn-link">About</button>
+        </div>
+    );
+};
+
+export default Footer;
